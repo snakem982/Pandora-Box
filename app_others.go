182cce43b0b4d17daee8a13c77efeea214a1ce18
snakem982 <snakem982@gmail.com>
2024-06-20 19:10:39 +0800

    Open Source

---
+++ app_others.go
@@ -0,0 +1,68 @@
+//go:build !darwin
+
+package main
+
+import (
+	"context"
+	"github.com/metacubex/mihomo/log"
+	isadmin "pandora-box/backend/system/admin"
+	"pandora-box/backend/system/open"
+	"runtime"
+)
+
+// App struct
+type App struct {
+	ctx   context.Context
+	addr  string
+	apiOk bool
+}
+
+func NewApp(addr string) *App {
+	return &App{
+		addr: addr,
+	}
+}
+
+func (a *App) startup(ctx context.Context) {
+	a.ctx = ctx
+}
+
+func (a *App) IsMac() string {
+	if runtime.GOOS == "darwin" {
+		return "true"
+	}
+
+	return "false"
+}
+
+func (a *App) GetMacAcStatus() string {
+	status, _ := GetAcStatus()
+	return status
+}
+
+func GetAcStatus() (string, string) {
+	return "1", ""
+}
+
+func (a *App) SetMacAc(pwd string) string {
+	return "1"
+}
+
+func (a *App) IsAdmin() string {
+	if isadmin.Check() {
+		return "true"
+	}
+
+	return "false"
+}
+
+func (a *App) GetFreePort() string {
+	return a.addr
+}
+
+func (a *App) OpenConfigDirectory() {
+	_, err := open.OpenConfigDirectory()
+	if err != nil {
+		log.Errorln("OpenConfigDirectory error:", err)
+	}
+}
