b99f45c4969150728449c7b5bf5e32a0a599d98b
snakem982 <snakem982@gmail.com>
2024-06-25 14:16:14 +0800

    chore: v0.2.12

--- backend/constant/constant.go
+++ backend/constant/constant.go
@@ -14,6 +14,6 @@ const (
 	CollectFuzzy     = "fuzzy"
 )
 
-const PandoraVersion = "v0.2.11"
+const PandoraVersion = "v0.2.12"
 const PandoraVersionUrl = "https://raw.githubusercontent.com/snakem982/Pandora-Box/main/version.txt"
 const PandoraDownloadUrl = "https://github.com/snakem982/Pandora-Box/releases/download/%s/%s-%s.zip"
