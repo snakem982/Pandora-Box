d2aeb3f494f81d6744da264ffabbdb361a5c6392
snakem982 <snakem982@gmail.com>
2024-12-09 18:59:49 +0800

    chore: upgrade dependencies

--- backend/system/update/update_windows.go
+++ backend/system/update/update_windows.go
@@ -3,12 +3,8 @@
 package update
 
 import (
-	"fmt"
-	"os"
-	"os/exec"
 	"pandora-box/backend/constant"
 	"pandora-box/backend/tools"
-	"time"
 )
 
 func IsNeedUpdate() (bool, string) {
@@ -22,20 +18,5 @@ func IsNeedUpdate() (bool, string) {
 }
 
 func Replace() bool {
-	err := os.Rename("./cms-test.exe", "./cms-test-temp.exe")
-	if err != nil {
-		fmt.Print(err)
-		return false
-	}
-	time.Sleep(5 * time.Second)
-	os.Rename("./cms.exe", "./cms-test.exe")
-	time.Sleep(2 * time.Second)
-	os.Remove("./cms-test-temp.exe")
-	time.Sleep(5 * time.Second)
-
-	// 到程序安装路径下去执行启动命令(预防相对路径方式启动)
-	daemon := "timeout /T 3 & E:\\cms\\cms-test.exe 2>&1 &"
-	_ = exec.Command("cmd.exe", "/C", daemon).Start()
-
 	return false
 }
