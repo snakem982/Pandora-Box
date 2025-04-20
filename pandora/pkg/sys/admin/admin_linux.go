03cf1390d5682cdfe1e25badea3055e01bc94b77
snakem982 <snakem982@gmail.com>
2025-04-20 12:15:48 +0000

    proxy

--- pandora/pkg/sys/admin/admin_linux.go
+++ pandora/pkg/sys/admin/admin_linux.go
@@ -14,20 +14,30 @@ func IsAdmin() bool {
 
 // KillProcessesByName 杀死所有名字为指定名称的进程
 func KillProcessesByName(name string) error {
-	// 使用 ps 和 grep 命令来查找进程
-	cmd := exec.Command("bash", "-c", fmt.Sprintf("ps aux | grep %s | grep -v grep | awk '{print $2}'", name))
-	output, err := cmd.Output()
-	if err != nil {
-		return err
+	// 确保目标进程名称不为空
+	if name == "" {
+		return fmt.Errorf("process name cannot be empty")
 	}
 
+	// 使用 ps 和 grep 查找进程，并确保精确匹配进程名称
+	cmd := exec.Command("bash", "-c", fmt.Sprintf("ps -eo pid,comm | grep -w %s | grep -v grep | awk '{print $1}'", name))
+	output, err := cmd.Output()
+	if err != nil {
+		return fmt.Errorf("failed to find processes with name %s: %w", name, err)
+	}
+
+	// 解析输出，获取所有匹配的进程 ID
 	pids := strings.Fields(string(output))
+	if len(pids) == 0 {
+		return fmt.Errorf("no processes found with name %s", name)
+	}
+
+	// 遍历所有进程 ID，逐个杀死
 	for _, pid := range pids {
-		// 使用 kill 命令来杀死进程
 		killCmd := exec.Command("kill", "-9", pid)
 		err := killCmd.Run()
 		if err != nil {
-			return err
+			return fmt.Errorf("failed to kill process with PID %s: %w", pid, err)
 		}
 	}
 
--- pandora/pkg/sys/proxy/proxy.go
+++ pandora/pkg/sys/proxy/proxy.go
@@ -1,17 +1,7 @@
 package sys
 
-import (
-	"fmt"
-	"github.com/snakem982/pandora-box/pandora/pkg/utils"
-)
-
 // EnableProxy 开启系统代理
 func EnableProxy(host string, port int) error {
-	// 检测端口，不可用进行报错
-	if !utils.IsPortAvailable(port) {
-		return fmt.Errorf("port %d is not available", port)
-	}
-
 	_ = OnHttp(Addr{
 		Host: host,
 		Port: port,
