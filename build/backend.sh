14ec02f144de249cb9165fd83b44fa37fe71a0d7
snakem982 <snakem982@gmail.com>
2025-05-16 14:34:12 +0800

    test: package

--- build/backend.sh
+++ build/backend.sh
@@ -1,3 +1,3 @@
 cd $(dirname $(readlink -f "$0"))
 cd ../src-go
-go build -tags=with_gvisor -trimpath -ldflags "-X github.com/snakem982/pandora-box/api.Version=v1.2.3" -o px
+go build -tags=with_gvisor -trimpath -ldflags "-X github.com/snakem982/pandora-box/api.Version=v-test" -o px
